0d4327d866a93f3ab21ff163a9aded9900516f98
aldwin.moutarlier <aldwin.moutarlier@oyez.fr>
2018-05-24 14:51:20 +0200

    init expo project

---
+++ Berlioz/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hey</Text>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
